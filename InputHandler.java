83f0f809737f8a0613dd9a87c888dd24aebfb8a7
lablonczy <lukeablonczy@hotmail.com>
2020-03-23 14:48:21 -0400

    Added JavaDoc to InputHandler class

--- InputHandler.java
+++ InputHandler.java
@@ -1,4 +1,4 @@
-package SomethingBasicCmdDesign;//lukacs ablonczy
+package SomethingBasicCmdDesign;
 
 import java.util.HashMap;
 
@@ -6,11 +6,19 @@ import java.util.HashMap;
  * @author Lukacs Ablonczy
  * @author Taylor Norton
  * @author Jyles Tygra
+ * <p>
+ * Defines Input handler, uses hash map to store valid commands and responses to them
  */
 public class InputHandler {
 
 	private HashMap<String, Command> commands;
 
+
+	/**
+	 * Constructor for InputHandler class, defines valid commands and responses to them
+	 *
+	 * @param document Document type on which to run the given command
+	 */
 	public InputHandler(Document document) {
 		commands = new HashMap<>();
 		commands.put("load", new LoadCommand(document));
@@ -19,6 +27,11 @@ public class InputHandler {
 		commands.put("print", new PrintCommand(document));
 	}
 
+	/**
+	 * Runs valid command given by param using hash lookup, exits method if not valid
+	 *
+	 * @param data the command to run
+	 */
 	public void inputEntered(String data) {
 		if (!isValid(data)) {
 			System.out.println("Sorry, we don't recognize that command");
@@ -28,6 +41,12 @@ public class InputHandler {
 		commands.get(data);
 	}
 
+	/**
+	 * Checks validity of command by comparing given string while ignoring case and leading spaces
+	 *
+	 * @param toCheck The command that will be checked for validity
+	 * @return Whether the given command is equal to any of the four valid commands
+	 */
 	private boolean isValid(String toCheck) {
 		toCheck = toCheck.trim().toLowerCase();
 
