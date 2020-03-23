6ab13bc6efc03cba6ab2a1ea2681f715722abb46
lablonczy <lukeablonczy@hotmail.com>
2020-03-23 14:36:34 -0400

    Added Driver, InputHandler java complete

--- InputHandler.java
+++ InputHandler.java
@@ -11,12 +11,27 @@ public class InputHandler {
 
 	private HashMap<String, Command> commands;
 
-	public InputHandler(Document document){
-
+	public InputHandler(Document document) {
+		commands = new HashMap<>();
+		commands.put("load", new LoadCommand(document));
+		commands.put("save", new SpellCommand(document));
+		commands.put("spell", new SaveCommand(document));
+		commands.put("print", new PrintCommand(document));
 	}
 
-	public void InputEntered(String data){
+	public void inputEntered(String data) {
+		if (!isValid(data)) {
+			System.out.println("Sorry, we don't recognize that command");
+			return;
+		}
 
+		commands.get(data);
+	}
+
+	private boolean isValid(String toCheck) {
+		toCheck = toCheck.trim().toLowerCase();
+
+		return toCheck.equals("load") || toCheck.equals("save") || toCheck.equals("spell") || toCheck.equals("print");
 	}
 
 }
